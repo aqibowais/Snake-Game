1a8b6f2c7d7cb7b9cac5ec9a3eb871b45176d970
Aqib Owais <owaisharoon60@gmail.com>
2024-05-24 08:57:56 +0500

    Snake Head

--- index.js
+++ index.js
@@ -74,7 +74,11 @@ const initGame = ()=>{
     }
     for (let i = 0; i < snakeBody.length; i++) {
         
-        htmlMarkup += `<div class = "snake" style = "grid-area : ${snakeBody[i][1]}/${snakeBody[i][0]}"></div>`
+        if (i === 0) {
+            htmlMarkup += `<div class="snake snake-head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+          } else {
+            htmlMarkup += `<div class="snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+          }
         if(i!==0 && snakeBody[0][1]===snakeBody[i][1] && snakeBody[0][0]===snakeBody[i][0]){
             gameOver=true
         }
